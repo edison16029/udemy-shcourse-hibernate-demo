eaa80e5aa897fe656bb91e8c47de286ec168519d
Edison <edison16029@gmail.com>
2021-11-05 15:26:51 +0530

    One to Many Uni mapping

---
+++ hb-04-one-to-many-uni/src/com/luv2code/hibernate/demo/GetInstructorDetailDemo.java
@@ -0,0 +1,53 @@
+package com.luv2code.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.luv2code.hibernate.demo.entity.Instructor;
+import com.luv2code.hibernate.demo.entity.InstructorDetail;
+import com.luv2code.hibernate.demo.entity.Student;
+
+public class GetInstructorDetailDemo {
+
+	public static void main(String[] args) {
+		
+		// create session factor
+		SessionFactory  factory = new Configuration()
+				.configure("hibernate.cfg.xml") //This is option as this is the default file name it checks for configuration
+				.addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class)
+				.buildSessionFactory();
+		
+		// create session
+		Session session = factory.getCurrentSession();
+		
+		try {
+			
+			session.beginTransaction();
+			
+			int theId = 44;
+			
+			InstructorDetail tempInstructorDetail = session.get(InstructorDetail.class, theId);
+			
+			System.out.println("tempInstructorDetail : " + tempInstructorDetail);
+			
+			System.out.println("the associated instructor " + tempInstructorDetail.getInstructor());
+			
+			session.getTransaction().commit();
+			
+			System.out.println("Donewe!");
+			
+		}
+		catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		finally {
+			session.close();
+			factory.close();
+		}
+
+	}
+
+}
